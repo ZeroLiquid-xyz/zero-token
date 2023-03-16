5c7c3dbeab819483f3db0338e6523d4b3dcad969
Zack <zack.zeroliquid@gmail.com>
2023-03-16 19:00:54 +0500

    feat: implement token

--- src/Mock.sol
+++ src/Mock.sol
@@ -1,8 +1,16 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.19;
 
-contract Foo {
-    function id(uint256 value) external pure returns (uint256) {
-        return value;
+import "openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+
+contract Mock is ERC20Burnable {
+    constructor(address[] memory addresses, uint256[] memory amounts) ERC20("Mock", "MOCK") {
+        _mintToAddresses(addresses, amounts);
+    }
+
+    function _mintToAddresses(address[] memory addresses, uint256[] memory amounts) internal {
+        for (uint256 i = 0; i < addresses.length; i++) {
+            _mint(addresses[i], amounts[i]);
+        }
     }
 }
