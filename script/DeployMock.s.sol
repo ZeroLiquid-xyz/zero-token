23b9a2114fc69992b8ff46e2b6a39edc8b43ae60
Zack <zack.zeroliquid@gmail.com>
2023-03-16 19:15:47 +0500

    test: implement unit tests

--- script/DeployMock.s.sol
+++ script/DeployMock.s.sol
@@ -2,12 +2,16 @@
 pragma solidity >=0.8.19;
 
 import { Script } from "forge-std/Script.sol";
-import { Foo } from "../src/Foo.sol";
+import { Mock } from "../src/Mock.sol";
 
 /// @dev See the Solidity Scripting tutorial: https://book.getfoundry.sh/tutorials/solidity-scripting
 contract DeployFoo is Script {
     address internal deployer;
-    Foo internal foo;
+    Mock internal mock;
+
+    address public constant FOUNDATION = 0xAfA13aa8F1b1d89454369c28b0CE1811961A7907;
+    address[] internal addresses = [FOUNDATION];
+    uint256[] internal amounts = [1e18];
 
     function setUp() public virtual {
         string memory mnemonic = vm.envString("MNEMONIC");
@@ -16,7 +20,7 @@ contract DeployFoo is Script {
 
     function run() public {
         vm.startBroadcast(deployer);
-        foo = new Foo();
+        mock = new Mock(addresses, amounts);
         vm.stopBroadcast();
     }
 }
--- test/Mock.t.sol
+++ test/Mock.t.sol
@@ -1,49 +1,260 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.19 <0.9.0;
 
-import { PRBTest } from "@prb/test/PRBTest.sol";
-import { console2 } from "forge-std/console2.sol";
-import { StdCheats } from "forge-std/StdCheats.sol";
+import "forge-std/Test.sol";
+import "./../src/Mock.sol";
+import "./../src/test-helpers/SigUtils.sol";
+import "./../src/test-helpers/Deposit.sol";
 
-interface IERC20 {
-    function balanceOf(address account) external view returns (uint256);
+contract ERC20Test is Test {
+    Mock internal token;
+    SigUtils internal sigUtils;
+
+    uint256 internal ownerPrivateKey;
+    uint256 internal spenderPrivateKey;
+
+    address internal owner;
+    address internal spender;
+
+    address public constant FOUNDATION = 0xAfA13aa8F1b1d89454369c28b0CE1811961A7907;
+    address[] internal addresses = [FOUNDATION];
+    uint256[] internal amounts = [1e18];
+
+    function setUp() public {
+        token = new Mock(addresses, amounts);
+        sigUtils = new SigUtils(token.getDomainSeparator());
+
+        ownerPrivateKey = 0xA11CE;
+        spenderPrivateKey = 0xB0B;
+
+        owner = vm.addr(ownerPrivateKey);
+        spender = vm.addr(spenderPrivateKey);
+
+        token.mint(owner, 1e18);
+    }
+
+    // testing permit
+    function test_Permit() public {
+        SigUtils.Permit memory permit =
+            SigUtils.Permit({owner: owner, spender: spender, value: 1e18, nonce: 0, deadline: 1 days});
+
+        bytes32 digest = sigUtils.getTypedDataHash(permit);
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(ownerPrivateKey, digest);
+
+        token.permit(permit.owner, permit.spender, permit.value, permit.deadline, v, r, s);
+
+        assertEq(token.allowance(owner, spender), 1e18);
+        assertEq(token.nonces(owner), 1);
+    }
+
+    // testing failure for calls with an expired deadline, invalid signer, invalid nonce & signature replay
+    function testRevert_ExpiredPermit() public {
+        SigUtils.Permit memory permit =
+            SigUtils.Permit({owner: owner, spender: spender, value: 1e18, nonce: token.nonces(owner), deadline: 1 days});
+
+        bytes32 digest = sigUtils.getTypedDataHash(permit);
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(ownerPrivateKey, digest);
+
+        vm.warp(1 days + 1 seconds); // fast forward one second past the deadline
+
+        vm.expectRevert("Mock:: AUTH_EXPIRED");
+        token.permit(permit.owner, permit.spender, permit.value, permit.deadline, v, r, s);
+    }
+
+    function testRevert_InvalidSigner() public {
+        SigUtils.Permit memory permit =
+            SigUtils.Permit({owner: owner, spender: spender, value: 1e18, nonce: token.nonces(owner), deadline: 1 days});
+
+        bytes32 digest = sigUtils.getTypedDataHash(permit);
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(spenderPrivateKey, digest); // spender signs owner's approval
+
+        vm.expectRevert("Mock:: INVALID_SIGNATURE");
+        token.permit(permit.owner, permit.spender, permit.value, permit.deadline, v, r, s);
+    }
+
+    function testRevert_InvalidNonce() public {
+        SigUtils.Permit memory permit = SigUtils.Permit({
+            owner: owner,
+            spender: spender,
+            value: 1e18,
+            nonce: 1, // owner nonce stored on-chain is 0
+            deadline: 1 days
+        });
+
+        bytes32 digest = sigUtils.getTypedDataHash(permit);
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(ownerPrivateKey, digest);
+
+        vm.expectRevert("Mock:: INVALID_SIGNATURE");
+        token.permit(permit.owner, permit.spender, permit.value, permit.deadline, v, r, s);
+    }
+
+    function testRevert_SignatureReplay() public {
+        SigUtils.Permit memory permit =
+            SigUtils.Permit({owner: owner, spender: spender, value: 1e18, nonce: 0, deadline: 1 days});
+
+        bytes32 digest = sigUtils.getTypedDataHash(permit);
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(ownerPrivateKey, digest);
+
+        token.permit(permit.owner, permit.spender, permit.value, permit.deadline, v, r, s);
+
+        vm.expectRevert("Mock:: INVALID_SIGNATURE");
+        token.permit(permit.owner, permit.spender, permit.value, permit.deadline, v, r, s);
+    }
+
+    // testing transferfrom
+    function test_TransferFromLimitedPermit() public {
+        SigUtils.Permit memory permit =
+            SigUtils.Permit({owner: owner, spender: spender, value: 1e18, nonce: 0, deadline: 1 days});
+
+        bytes32 digest = sigUtils.getTypedDataHash(permit);
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(ownerPrivateKey, digest);
+
+        token.permit(permit.owner, permit.spender, permit.value, permit.deadline, v, r, s);
+
+        vm.prank(spender);
+        token.transferFrom(owner, spender, 1e18);
+
+        assertEq(token.balanceOf(owner), 0);
+        assertEq(token.balanceOf(spender), 1e18);
+        assertEq(token.allowance(owner, spender), 0);
+    }
+
+    function test_TransferFromMaxPermit() public {
+        SigUtils.Permit memory permit =
+            SigUtils.Permit({owner: owner, spender: spender, value: type(uint256).max, nonce: 0, deadline: 1 days});
+
+        bytes32 digest = sigUtils.getTypedDataHash(permit);
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(ownerPrivateKey, digest);
+
+        token.permit(permit.owner, permit.spender, permit.value, permit.deadline, v, r, s);
+
+        vm.prank(spender);
+        token.transferFrom(owner, spender, 1e18);
+
+        assertEq(token.balanceOf(owner), 0);
+        assertEq(token.balanceOf(spender), 1e18);
+        assertEq(token.allowance(owner, spender), type(uint256).max);
+    }
+
+    // testing failure for calls with and invalid allowance & balance
+    function testFail_InvalidAllowance() public {
+        SigUtils.Permit memory permit = SigUtils.Permit({
+            owner: owner,
+            spender: spender,
+            value: 5e17, // approve only 0.5 tokens
+            nonce: 0,
+            deadline: 1 days
+        });
+
+        bytes32 digest = sigUtils.getTypedDataHash(permit);
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(ownerPrivateKey, digest);
+
+        token.permit(permit.owner, permit.spender, permit.value, permit.deadline, v, r, s);
+
+        vm.prank(spender);
+        token.transferFrom(owner, spender, 1e18); // attempt to transfer 1 token
+    }
+
+    function testFail_InvalidBalance() public {
+        SigUtils.Permit memory permit = SigUtils.Permit({
+            owner: owner,
+            spender: spender,
+            value: 2e18, // approve 2 tokens
+            nonce: 0,
+            deadline: 1 days
+        });
+
+        bytes32 digest = sigUtils.getTypedDataHash(permit);
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(ownerPrivateKey, digest);
+
+        token.permit(permit.owner, permit.spender, permit.value, permit.deadline, v, r, s);
+
+        vm.prank(spender);
+        token.transferFrom(owner, spender, 2e18); // attempt to transfer 2 tokens (owner only owns 1)
+    }
 }
 
-/// @dev See the "Writing Tests" section in the Foundry Book if this is your first time with Forge.
-/// https://book.getfoundry.sh/forge/writing-tests
-contract FooTest is PRBTest, StdCheats {
-    /// @dev An optional function invoked before each test case is run
+contract DepositTest is Test {
+    Deposit internal deposit;
+    Mock internal token;
+    SigUtils internal sigUtils;
+
+    uint256 internal ownerPrivateKey;
+    address internal owner;
+
+    address public constant FOUNDATION = 0xAfA13aa8F1b1d89454369c28b0CE1811961A7907;
+    address[] internal addresses = [FOUNDATION];
+    uint256[] internal amounts = [1e18];
+
     function setUp() public {
-        // solhint-disable-previous-line no-empty-blocks
+        deposit = new Deposit();
+        token = new Mock(addresses, amounts);
+        sigUtils = new SigUtils(token.getDomainSeparator());
+
+        ownerPrivateKey = 0xA11CE;
+        owner = vm.addr(ownerPrivateKey);
+
+        token.mint(owner, 1e18);
     }
 
-    /// @dev Simple test. Run Forge with `-vvvv` to see console logs.
-    function test_Example() external {
-        console2.log("Hello World");
-        assertTrue(true);
+    function test_DepositWithLimitedPermit() public {
+        SigUtils.Permit memory permit = SigUtils.Permit({
+            owner: owner,
+            spender: address(deposit),
+            value: 1e18,
+            nonce: token.nonces(owner),
+            deadline: 1 days
+        });
+
+        bytes32 digest = sigUtils.getTypedDataHash(permit);
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(ownerPrivateKey, digest);
+
+        deposit.depositWithPermit(
+            address(token), 1e18, permit.owner, permit.spender, permit.value, permit.deadline, v, r, s
+        );
+
+        assertEq(token.balanceOf(owner), 0);
+        assertEq(token.balanceOf(address(deposit)), 1e18);
+
+        assertEq(token.allowance(owner, address(deposit)), 0);
+        assertEq(token.nonces(owner), 1);
+
+        assertEq(deposit.userDeposits(owner, address(token)), 1e18);
     }
 
-    /// @dev Test that fuzzes an unsigned integer.
-    function testFuzz_Example(uint256 x) external {
-        vm.assume(x != 0);
-        assertGt(x, 0);
-    }
+    function test_DepositWithMaxPermit() public {
+        SigUtils.Permit memory permit = SigUtils.Permit({
+            owner: owner,
+            spender: address(deposit),
+            value: type(uint256).max,
+            nonce: token.nonces(owner),
+            deadline: 1 days
+        });
 
-    /// @dev Test that runs against a fork of Ethereum Mainnet. You need to set `API_KEY_ALCHEMY` in your environment
-    /// for this test to run - you can get an API key for free at https://alchemy.com.
-    function testFork_Example() external {
-        string memory alchemyApiKey = vm.envOr("API_KEY_ALCHEMY", string(""));
-        // Silently pass this test if the user didn't define the API key.
-        if (bytes(alchemyApiKey).length == 0) {
-            return;
-        }
+        bytes32 digest = sigUtils.getTypedDataHash(permit);
 
-        // Run the test normally, otherwise.
-        vm.createSelectFork({ urlOrAlias: "ethereum", blockNumber: 16_428_000 });
-        address usdc = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
-        address holder = 0x7713974908Be4BEd47172370115e8b1219F4A5f0;
-        uint256 actualBalance = IERC20(usdc).balanceOf(holder);
-        uint256 expectedBalance = 196_307_713.810457e6;
-        assertEq(actualBalance, expectedBalance);
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(ownerPrivateKey, digest);
+
+        deposit.depositWithPermit(
+            address(token), 1e18, permit.owner, permit.spender, permit.value, permit.deadline, v, r, s
+        );
+
+        assertEq(token.balanceOf(owner), 0);
+        assertEq(token.balanceOf(address(deposit)), 1e18);
+
+        assertEq(token.allowance(owner, address(deposit)), type(uint256).max);
+        assertEq(token.nonces(owner), 1);
+
+        assertEq(deposit.userDeposits(owner, address(token)), 1e18);
     }
 }
