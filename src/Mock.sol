6af034b01ca885066858124337715d39ba6f5302
Zack <zack.zeroliquid@gmail.com>
2023-03-16 19:05:00 +0500

    feat: implement eip-3009 transfer with authorization

--- src/Mock.sol
+++ src/Mock.sol
@@ -16,8 +16,19 @@ contract Mock is ERC20Burnable {
     // keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
     bytes32 public constant PERMIT_TYPEHASH = 0x6e71edae12b1b97f4d1f60370fef10105fa2faae0126114a169c64845d6126c9;
 
+    // keccak256("TransferWithAuthorization(address from,address to,uint256 value,uint256 validAfter,uint256
+    // validBefore,bytes32 nonce)");
+    bytes32 public constant TRANSFER_WITH_AUTHORIZATION_TYPEHASH =
+        0x7c7c6cdb67a18743f49ec6fa9b35f50d52ed05cbed4cc592e13b44501c1a2267;
+
     // ERC-2612 state
     mapping(address => uint256) public nonces;
+
+    // ERC-3009 state
+    mapping(address => mapping(bytes32 => bool)) public authorizationState;
+
+    // ERC-3009 event
+    event AuthorizationUsed(address indexed authorizer, bytes32 indexed nonce);
 
     constructor(address[] memory addresses, uint256[] memory amounts) ERC20("Mock", "MOCK") {
         _mintToAddresses(addresses, amounts);
@@ -67,5 +78,32 @@ contract Mock is ERC20Burnable {
         _validateSignedData(owner, encodeData, v, r, s);
 
         _approve(owner, spender, value);
+    }
+
+    function transferWithAuthorization(
+        address from,
+        address to,
+        uint256 value,
+        uint256 validAfter,
+        uint256 validBefore,
+        bytes32 nonce,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    )
+        external
+    {
+        require(block.timestamp > validAfter, "Mock:: AUTH_NOT_YET_VALID");
+        require(block.timestamp < validBefore, "Mock:: AUTH_EXPIRED");
+        require(!authorizationState[from][nonce], "Mock:: AUTH_ALREADY_USED");
+
+        bytes32 encodeData =
+            keccak256(abi.encode(TRANSFER_WITH_AUTHORIZATION_TYPEHASH, from, to, value, validAfter, validBefore, nonce));
+        _validateSignedData(from, encodeData, v, r, s);
+
+        authorizationState[from][nonce] = true;
+        emit AuthorizationUsed(from, nonce);
+
+        _transfer(from, to, value);
     }
 }
